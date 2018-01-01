162cf3ede7b2b6d3c755a011651a5d153e5c9ae0
Qathom <tony9295@gmail.com>
2018-01-01 17:23:45 +0100

    Add events

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,23 @@ export default {
    * time to finish the game in seconds
    */
   time: 60,
+
   /**
    * default score when the game starts
    */
-  score: 0
+  score: 0,
+
+  /**
+   * callback event when the game starts
+   */
+  onStart: () => {
+
+  },
+
+  /**
+   * callback event when the game finishes
+   */
+  onFinish: () => {
+
+  }
 }
