1be781ac2603072d98c4a26aa3449c7c1457a255
Qathom <tony9295@gmail.com>
2018-01-01 17:23:40 +0100

    Add configurable text

--- src/data/resultView.js
+++ src/data/resultView.js
@@ -1,6 +1,8 @@
 export default {
   title: 'Your score is :score',
   results: 'Results',
+  yourAnswer: 'Your answer',
+  expectedAnswer: 'Good or best answer',
   css: {
     layout: {
       backgroundColor: '#111'
